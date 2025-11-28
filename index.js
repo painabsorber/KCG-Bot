2bc937c9a864234fddddbf45d5cbd1d7e0a686ef
painabsorber <c2n3jcxb@gmail.com>
2025-11-28 16:35:55 +0900

    Fix: Chane Port

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ client.login(process.env.DISCORD_TOKEN);
 // Koyeb用簡易Webサーバー（ヘルスチェック用）
 const app = express();
 app.get('/', (req, res) => res.send('Bot is running!'));
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
